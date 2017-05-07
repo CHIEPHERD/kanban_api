0ad04fd9339533334b7ed28a11334d87027f6e89
amelie certin <amelie.certin@gmail.com>
2017-05-07 20:30:57 +0200

    Create project from amqp

---
+++ jobs/projects/create.js
@@ -0,0 +1,31 @@
+const models = require('../../models');
+let Project = models.projects;
+
+module.exports = function(connection, done) {
+  connection.createChannel(function(err, ch) {
+    console.log(err);
+    var ex = 'chiepherd.project.created';
+    ch.assertExchange(ex, 'fanout', { durable: false });
+    ch.assertQueue('chiepherd.project.created', { exclusive: false }, function(err, q) {
+      ch.bindQueue(q.queue, ex, 'chiepherd.project.created')
+
+      ch.consume(q.queue, function(msg) {
+        // LOG
+        console.log(" [%s]: %s", msg.fields.routingKey, msg.content.toString());
+        let json = JSON.parse(msg.content.toString());
+
+        // Create project
+        Project.create({
+          name: json.name,
+          label: json.label
+        }).then(function(project) {
+          console.log('OK');
+        }).catch(function(error) {
+          console.log(error);
+          console.log('NOK');
+        });
+      }, { noAck: true });
+    });
+  });
+  done();
+}
