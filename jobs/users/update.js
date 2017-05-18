60d7b18213031b2ca6d4690885946780e646fac9
amelie certin <amelie.certin@gmail.com>
2017-05-18 12:45:50 +0200

    Add classic actions

--- jobs/users/update.js
+++ jobs/users/update.js
@@ -4,8 +4,8 @@ let User = models.users;
 module.exports = function(connection, done) {
   connection.createChannel(function(err, ch) {
     console.log(err);
-    var ex = 'chiepherd.user.created';
-    var queue = 'kanban.user.create';
+    var ex = 'chiepherd.user.updated';
+    var queue = 'kanban.user.update';
     ch.assertExchange(ex, 'fanout', { durable: false });
     ch.assertQueue(queue, { exclusive: false }, function(err, q) {
       ch.bindQueue(q.queue, ex, queue);
@@ -15,13 +15,24 @@ module.exports = function(connection, done) {
         console.log(" [%s]: %s", queue, msg.content.toString());
         let json = JSON.parse(msg.content.toString());
 
-        // Create user
-        User.create({
-          email: json.email,
-          firstname: json.firstname,
-          lastname: json.lastname
+        // Update user
+        User.find({
+          where: {
+            email: json.email
+          }
         }).then(function(user) {
-          console.log('OK');
+          if(user) {
+            user.update({
+              lastname: json.lastname,
+              firstname: json.firstname,
+              nickname: json.nickname
+            }).then(function(user) {
+              console.log('OK');
+            }).catch(function(error) {
+              console.log(error);
+              console.log('NOK');
+            });
+          }
         }).catch(function(error) {
           console.log(error);
           console.log('NOK');
