3af52afe3a4d8b9f9c5a7c24fcebc388f54fa357
amelie certin <amelie.certin@gmail.com>
2017-07-03 10:41:25 +0200

    Update a projectAssignment

---
+++ jobs/project_assignments/update.js
@@ -0,0 +1,36 @@
+const models = require('../../models');
+let ProjectAssignment = models.project_assignments;
+
+module.exports = function(connection, done) {
+  connection.createChannel(function(err, ch) {
+    console.log(err);
+    var ex = 'chiepherd.project_assignment.updated';
+    var queue = 'kanban.project_assignment.update';
+    ch.assertExchange(ex, 'fanout', { durable: false });
+    ch.assertQueue(queue, { exclusive: false }, function(err, q) {
+      ch.bindQueue(q.queue, ex, queue);
+
+      ch.consume(q.queue, function(msg) {
+        // LOG
+        console.log(" [%s]: %s", queue, msg.content.toString());
+        let json = JSON.parse(msg.content.toString());
+
+        // Create project_assignment
+        ProjectAssignment.find({
+          uuid: json.uuid
+        }).then(function(project_assignment) {
+          project_assignment.update({
+            projectId: json.projectId || this.projectId
+          }).then(function (assignment) {
+            console.log('OK');
+            console.log(assignment);
+          })
+        }).catch(function(error) {
+          console.log(error);
+          console.log('NOK');
+        });
+      }, { noAck: true });
+    });
+  });
+  done();
+}
