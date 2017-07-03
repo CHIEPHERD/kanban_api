c953da6d4b65256e74f86c2173df06a073c79725
amelie certin <amelie.certin@gmail.com>
2017-07-03 19:43:53 +0200

    List all the states of a project

---
+++ jobs/projects/states.js
@@ -0,0 +1,60 @@
+const models = require('../../models');
+let Project = models.projects;
+let State = models.states;
+
+module.exports = function(connection, done) {
+  connection.createChannel(function(err, ch) {
+    console.log(err);
+    var ex = 'kanban.main';
+    ch.assertExchange(ex, 'topic');
+    ch.assertQueue('kanban.project.states', { exclusive: false }, function(err, q) {
+      ch.bindQueue(q.queue, ex, "kanban.project.states")
+
+      ch.consume(q.queue, function(msg) {
+        // LOG
+        console.log(" [%s]: %s", msg.fields.routingKey, msg.content.toString());
+        let json = JSON.parse(msg.content.toString());
+
+        Project.find({
+          where: {
+            uuid: json.projectId
+          }
+        }).then(function (project) {
+          if (project != undefined) {
+            State.findAll({
+              where: {
+                projectId: project.id
+              }
+            }).then(function (states) {
+              for (var i = 0; i < states.length; i++) {
+                states[i] = states[i].responsify();
+              }
+              ch.sendToQueue(msg.properties.replyTo,
+                new Buffer.from(JSON.stringify(states)),
+                { correlationId: msg.properties.correlationId });
+              ch.ack(msg);
+            }).catch(function (error) {
+              console.log(error);
+              ch.sendToQueue(msg.properties.replyTo,
+                new Buffer(error.toString()),
+                { correlationId: msg.properties.correlationId });
+              ch.ack(msg);
+            });
+          } else {
+            ch.sendToQueue(msg.properties.replyTo,
+              new Buffer("Unknown project."),
+              { correlationId: msg.properties.correlationId });
+            ch.ack(msg);
+          }
+        }).catch(function (error) {
+          console.log(error);
+          ch.sendToQueue(msg.properties.replyTo,
+            new Buffer(error.toString()),
+            { correlationId: msg.properties.correlationId });
+          ch.ack(msg);
+        });
+      }, { noAck: false });
+    });
+  });
+  done();
+}
