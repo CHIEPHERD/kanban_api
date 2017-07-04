c0fa37fbc667e43210112adaab2ddd4af956a38f
amelie certin <amelie.certin@gmail.com>
2017-07-04 14:37:00 +0200

    Create task with 0 points in Icebox

--- jobs/tasks/create.js
+++ jobs/tasks/create.js
@@ -1,6 +1,7 @@
 const models = require('../../models');
 let Task = models.tasks;
 let Project = models.projects;
+let State = models.states;
 
 module.exports = function(connection, done) {
   connection.createChannel(function(err, ch) {
@@ -28,16 +29,30 @@ module.exports = function(connection, done) {
             }
           }).then(function (ancestor) {
             if (json.ancestorUuid == null || ancestor != null) {
-              Task.create({
-                uuid: json.uuid,
-                title: json.title,
-                description: json.description,
-                type: json.label,
+              State.find({
                 projectId: project.id,
-                ancestorId: ancestor && ancestor.id
-              }).then(function(task) {
-                console.log('OK');
-              }).catch(function(error) {
+                name: 'Icebox'
+              }).then(function (state) {
+                if (state != undefined) {
+                  Task.create({
+                    uuid: json.uuid,
+                    title: json.title,
+                    description: json.description,
+                    type: json.label,
+                    projectId: project.id,
+                    ancestorId: ancestor && ancestor.id
+                    stateId: state.id
+                  }).then(function(task) {
+                    console.log('OK');
+                  }).catch(function(error) {
+                    console.log(error);
+                    console.log('NOK');
+                  });
+                } else {
+                  console.log('Invalid state');
+                  console.log('NOK');
+                }
+              }).catch(function (error) {
                 console.log(error);
                 console.log('NOK');
               });
