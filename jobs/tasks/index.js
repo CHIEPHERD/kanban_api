066c94353addfa1dab79aa84a49f86e9a3f8e2fa
amelie certin <amelie.certin@gmail.com>
2017-07-04 11:33:47 +0200

    Move a task

--- jobs/tasks/index.js
+++ jobs/tasks/index.js
@@ -2,5 +2,6 @@ module.exports = {
   create: require('./create'),
   delete: require('./delete'),
   update: require('./update'),
-  comments: require('./comments')
+  comments: require('./comments'),
+  move: require('./move')
 }
